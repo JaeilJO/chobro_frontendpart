f727c370777418bbde8d1c614ff42254a109a4c5
JaeilJo <whwodlf97@gmail.com>
2023-05-01 06:36:04 +0900

    feat: create Store for modal
    - create authModlaSlice for modal mode change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 import GlobalStyles from '../styles/GlobalStyle';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <ThemeProvider theme={theme}>
-                <GlobalStyles />
-                <Component {...pageProps} />
-            </ThemeProvider>
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <GlobalStyles />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </Provider>
         </>
     );
 }
